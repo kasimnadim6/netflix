49e4120d3b202f0f3a9add18ae1d4cf60ae6bcff
Kasim <kasimnadim6@gmail.com>
2022-04-09 12:44:16 +0530

    initial setup changes

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCUY-_txk-LsMa_nJYArFGFJZkh3PMF28M',
+  authDomain: 'netflix-de158.firebaseapp.com',
+  projectId: 'netflix-de158',
+  storageBucket: 'netflix-de158.appspot.com',
+  messagingSenderId: '720636866781',
+  appId: '1:720636866781:web:915002b673cd81a867eb34',
+  measurementId: 'G-Z46FKBNZN6',
+};
