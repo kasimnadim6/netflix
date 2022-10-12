599c2a052aa5056fb0f9b62fc9812d19bb79aaf9
Kasim Nadim <37031076+kasimnadim6@users.noreply.github.com>
2022-10-12 22:31:51 +0530

    issue fix

--- src/App.js
+++ src/App.js
@@ -5,24 +5,29 @@ import { Routes, Route } from 'react-router-dom';
 import { auth } from './firebase';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { login, logout, selectUser, startLoader } from './features/authSlice';
+import {
+  login,
+  logout,
+  selectUser,
+  startLoader,
+  stopLoader,
+} from './features/authSlice';
 import db from './firebase';
 import { collection, getDocs } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 import ProfileScreen from './screens/ProfileScreen';
 import NotFound from './components/NotFound';
-import Loader from './components/Loader';
 import {
   addSubscription,
   selectSubscription,
-  stopLoader,
 } from './features/subscriptionSlice';
+import Loader from './components/Loader';
 
 function App() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { user } = useSelector(selectUser);
-  const { isLoading, isSubscribed } = useSelector(selectSubscription);
+  const { user, isLoading } = useSelector(selectUser);
+  const { isSubscribed } = useSelector(selectSubscription);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
@@ -62,19 +67,18 @@ function App() {
     user?.uid && !isSubscribed && getSubscriptions();
   }, [dispatch, navigate, user, isSubscribed]);
   return (
-    <>
-      {isLoading || !user ? (
-        <Loader />
-      ) : (
-        <div className={styles.app}>
-          <Routes>
-            <Route path="/" element={user ? <HomeScreen /> : <LoginScreen />} />
-            <Route path="/profile" element={<ProfileScreen />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      )}
-    </>
+    <div className={styles.app}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            user ? <HomeScreen /> : isLoading ? <Loader /> : <LoginScreen />
+          }
+        />
+        <Route path="/profile" element={<ProfileScreen />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
